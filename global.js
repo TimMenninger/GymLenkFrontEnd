fc7b8cf950561503379ad78b2aff810e0fdaa1f5
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2021-05-03 20:59:12 +0000

    removed <script> from global.js

--- global.js
+++ global.js
@@ -3,78 +3,76 @@
 // https://gts-webflow-playground.webflow.io/samples/fetch-data-from-external-api-to-webflow
 //
 
-<script>
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // CONSTANTS
-    //
+////////////////////////////////////////////////////////////////////////////
+//
+// CONSTANTS
+//
 
-    // Backend URL
-    const host = "127.0.0.1";
-    const port = "8181";
-    const backend_URL = "http://" + host + ":" + port;
+// Backend URL
+const host = "127.0.0.1";
+const port = "8181";
+const backend_URL = "http://" + host + ":" + port;
 
-    // Backend Endpoints
-    const BE_sign_in = "/sign-in";
-    const BE_create_account = "/create-account";
+// Backend Endpoints
+const BE_sign_in = "/sign-in";
+const BE_create_account = "/create-account";
 
-    // Frontend URL
-    const frontend_URL = "gymlenk-dashboard.webflow.io"
+// Frontend URL
+const frontend_URL = "gymlenk-dashboard.webflow.io"
 
-    // Frontend Pages
-    const FE_mygym = "/mygym";
-    const FE_dashboard = "/dashboard";
-    const FE_onboarding = "/onboarding-2";
-    const FE_onboarding_aboutmygym = "/onboarding/aboutmygym";
-    const FE_onboarding_amenities = "/onboarding/amenities";
-    const FE_onboarding_links = "/onboarding/links";
-    const FE_onboarding_getstarted = "/onboarding/getstarted";
-    const FE_login = "/login";
+// Frontend Pages
+const FE_mygym = "/mygym";
+const FE_dashboard = "/dashboard";
+const FE_onboarding = "/onboarding-2";
+const FE_onboarding_aboutmygym = "/onboarding/aboutmygym";
+const FE_onboarding_amenities = "/onboarding/amenities";
+const FE_onboarding_links = "/onboarding/links";
+const FE_onboarding_getstarted = "/onboarding/getstarted";
+const FE_login = "/login";
 
-    // Request Headers
-    const HDR_content_type_json = "application/json; charset=UTF-8";
+// Request Headers
+const HDR_content_type_json = "application/json; charset=UTF-8";
 
-    // Other URLs
-    const URL_landing_after_login = FE_mygym;
-    const URL_landing_after_signup = FE_onboarding;
-    const URL_landing_after_onboarding = FE_onboarding_aboutmygym;
-    const URL_landing_after_onboarding_aboutmygym = FE_onboarding_amenities;
-    const URL_landing_after_onboarding_amenities = FE_onboarding_links;
-    const URL_landing_after_onboarding_links = FE_onboarding_getstarted
-    const URL_landing_after_onboarding_getstarted = "";
-    const URL_landing_after_logout = FE_login;
+// Other URLs
+const URL_landing_after_login = FE_mygym;
+const URL_landing_after_signup = FE_onboarding;
+const URL_landing_after_onboarding = FE_onboarding_aboutmygym;
+const URL_landing_after_onboarding_aboutmygym = FE_onboarding_amenities;
+const URL_landing_after_onboarding_amenities = FE_onboarding_links;
+const URL_landing_after_onboarding_links = FE_onboarding_getstarted
+const URL_landing_after_onboarding_getstarted = "";
+const URL_landing_after_logout = FE_login;
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // COMMON
-    //
+////////////////////////////////////////////////////////////////////////////
+//
+// COMMON
+//
 
-    function clearState() {
-        localStorage.clear();
-        window.location.replace(URL_landing_after_logout);
-    }
+function clearState() {
+    localStorage.clear();
+    window.location.replace(URL_landing_after_logout);
+}
 
-    function loggedIn() {
-        return (localStorage.getItem("dashboard") !== null)
-            && (localStorage.getItem("logged_in") === "true");
-    }
+function loggedIn() {
+    return (localStorage.getItem("dashboard") !== null)
+        && (localStorage.getItem("logged_in") === "true");
+}
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // NAVBAR
-    //
+////////////////////////////////////////////////////////////////////////////
+//
+// NAVBAR
+//
 
-    document.getElementById("navbar-log-out").addEventListener("click", function() {
-        clearState();
-    })
+document.getElementById("navbar-log-out").addEventListener("click", function() {
+    clearState();
+})
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // SIDEBAR
-    //
+////////////////////////////////////////////////////////////////////////////
+//
+// SIDEBAR
+//
 
-    document.getElementById("sidebar-log-out").addEventListener("click", function() {
-        clearState();
-    })
+document.getElementById("sidebar-log-out").addEventListener("click", function() {
+    clearState();
+})
 
-</script>
