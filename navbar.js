cf88fe0231503a2844bbdf7b5bfeac47f19cc487
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2021-05-05 20:06:23 +0000

    uncommented navbar logout

---
+++ navbar.js
@@ -0,0 +1,6 @@
+// Log out when navbar logout is pressed
+document.getElementById("navbar-log-out").addEventListener("click", function() {
+    clearState();
+})
+
+
---
+++ sidebar.js
@@ -0,0 +1,4 @@
+// Log out when sidebar log out is clicked
+document.getElementById("sidebar-log-out").addEventListener("click", function() {
+    clearState();
+})
