6abd0e79f1cad43b4104a06bf6e1921c1747318f
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2021-05-09 15:58:21 +0000

    moved on ready function to bottom of aboutyourgym

--- onboarding/aboutmygym.js
+++ onboarding/aboutmygym.js
@@ -1,59 +1,3 @@
-$(document).ready(function() {
-    // Shorthand
-    const days_of_week = [ "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" ]
-
-    // Load the info that we have so far and replace items (if applicable)
-    if (localStorage.getItem("onboard_info") !== null) {
-        var data = JSON.parse(localStorage.getItem("onboard_info"));
-
-        // Get gym info
-        document.getElementById("onboarding-gym-name").value = data["organization_name"];
-        document.getElementById("onboarding-location-name").value = data["location_name"];
-        document.getElementById("onboarding-address1").value = data["physical_address"]["line1"];
-        document.getElementById("onboarding-address2").value = data["physical_address"]["line2"];
-        document.getElementById("onboarding-zip").value = data["physical_address"]["zip"];
-        document.getElementById("onboarding-phone").value = formatPhoneNumber(data["phone"]);
-        document.getElementById("onboarding-email").value = data["email"];
-        document.getElementById("onboarding-description").value = data["description"];
-
-        console.log(data)
-        for (const dow of days_of_week) {
-            var is_24h = false;
-            var is_closed = false;
-            var open = "";
-            var close = "";
-            if (data["hours"][dow].length == 1) {
-                if (data["hours"][dow][0] == 0) {
-                    is_24h = true;
-                } else if (data["hours"][dow][0] < 0) {
-                    is_closed = true;
-                }
-            }
-            if (!is_24h && !is_closed && data["hours"][dow].length >= 1) {
-                open = (data["hours"][dow][0] / 60).toString() + ":" + (data["hours"][dow][0] % 60);
-            }
-            if (data["hours"][dow].length > 1) {
-                close = (data["hours"][dow][1] / 60).toString() + ":" + (data["hours"][dow][1] % 60);
-            }
-
-            document.getElementById("onboarding-hours-" + dow + "-open").value = open;
-            document.getElementById("onboarding-hours-" + dow + "-close").disabled = close;
-
-            if (document.getElementById("24h-" + dow + "-onboarding-checkbox").checked != is_24h) {
-                document.getElementById("24h-" + dow + "-onboarding-checkbox").click();
-            }
-            if (document.getElementById("closed-" + dow + "-onboarding-checkbox").checked != is_closed) {
-                document.getElementById("closed-" + dow + "-onboarding-checkbox").click();
-            }
-            if (open !== "" || close !== "") {
-                document.getElementById("24h-" + dow + "-onboarding-checkbox").disabled    = true;
-                document.getElementById("closed-" + dow + "-onboarding-checkbox").disabled = true;
-            }
-
-        }
-    }
-});
-
 // Format phone number
 document.getElementById("onboarding-phone").addEventListener("keydown", enforceFormat);
 document.getElementById("onboarding-phone").addEventListener("keyup", formatToPhone);
@@ -129,3 +73,60 @@ document.getElementById("onboarding-about-continue-button").addEventListener("cl
     localStorage.setItem("onboarding_aboutyourgym", "done")
     window.location.assign(URL_landing_after_onboarding_aboutmygym);
 });
+
+$(document).ready(function() {
+    // Shorthand
+    const days_of_week = [ "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" ]
+
+    // Load the info that we have so far and replace items (if applicable)
+    if (localStorage.getItem("onboard_info") !== null) {
+        var data = JSON.parse(localStorage.getItem("onboard_info"));
+
+        // Get gym info
+        document.getElementById("onboarding-gym-name").value = data["organization_name"];
+        document.getElementById("onboarding-location-name").value = data["location_name"];
+        document.getElementById("onboarding-address1").value = data["physical_address"]["line1"];
+        document.getElementById("onboarding-address2").value = data["physical_address"]["line2"];
+        document.getElementById("onboarding-zip").value = data["physical_address"]["zip"];
+        document.getElementById("onboarding-phone").value = formatPhoneNumber(data["phone"]);
+        document.getElementById("onboarding-email").value = data["email"];
+        document.getElementById("onboarding-description").value = data["description"];
+
+        console.log(data)
+        for (const dow of days_of_week) {
+            var is_24h = false;
+            var is_closed = false;
+            var open = "";
+            var close = "";
+            if (data["hours"][dow].length == 1) {
+                if (data["hours"][dow][0] == 0) {
+                    is_24h = true;
+                } else if (data["hours"][dow][0] < 0) {
+                    is_closed = true;
+                }
+            }
+            if (!is_24h && !is_closed && data["hours"][dow].length >= 1) {
+                open = (data["hours"][dow][0] / 60).toString() + ":" + (data["hours"][dow][0] % 60);
+            }
+            if (data["hours"][dow].length > 1) {
+                close = (data["hours"][dow][1] / 60).toString() + ":" + (data["hours"][dow][1] % 60);
+            }
+
+            document.getElementById("onboarding-hours-" + dow + "-open").value = open;
+            document.getElementById("onboarding-hours-" + dow + "-close").disabled = close;
+
+            if (document.getElementById("24h-" + dow + "-onboarding-checkbox").checked != is_24h) {
+                document.getElementById("24h-" + dow + "-onboarding-checkbox").click();
+            }
+            if (document.getElementById("closed-" + dow + "-onboarding-checkbox").checked != is_closed) {
+                document.getElementById("closed-" + dow + "-onboarding-checkbox").click();
+            }
+            if (open !== "" || close !== "") {
+                document.getElementById("24h-" + dow + "-onboarding-checkbox").disabled    = true;
+                document.getElementById("closed-" + dow + "-onboarding-checkbox").disabled = true;
+            }
+
+        }
+    }
+});
+
