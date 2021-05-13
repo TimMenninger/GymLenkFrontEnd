575a09d1745d221adac4e1631223daedee5ec328
Tim Menninger <11710901+TimMenninger@users.noreply.github.com>
2021-05-12 17:14:28 -0700

    forgot password pages

---
+++ forgotpassword.js
@@ -0,0 +1,35 @@
+document.getElementById("gym-pwreset-button").addEventListener("submit", function() {
+    // Get email
+    var email = document.getElementById("gym-pwreset-email").value;
+
+    // Create a request variable and assign a new XMLHttpRequest object to
+    // it.
+    var request = new XMLHttpRequest();
+
+    // Open a new connection, using the POST request on the URL endpoint
+    request.open("POST", backend_URL + BE_sign_in, true);
+    request.setRequestHeader("Content-Type", HDR_content_type_json);
+    request.withCredentials = true;
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            if (request.status != 200) {
+                console.log("Request failed");
+                return;
+            }
+
+            // Begin accessing JSON data here
+            var data = JSON.parse(request.responseText);
+            if (!data["success"]) {
+                console.log(data["message"]);
+                return;
+            }
+
+            alert("Password link emailed")
+        }
+    }
+
+    // Send request
+    request.send(JSON.stringify({
+        "email"    : email
+    }));
+});
