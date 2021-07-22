ba6acc928e0df32ebb8a3d03918ec1c2bbf519ee
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2021-07-22 01:17:07 +0000

    finding where to remove original row

--- profile/members.js
+++ profile/members.js
@@ -25,14 +25,16 @@ $(document).ready(function() {
 
                 // Duplicate the template and remove it from the list
                 var table = document.getElementById("my-members-list-div");
-                var template_row = document.getElementById("my-members-row-div").cloneNode(true);
-                template_row.id = template_row.id + "-template";
+                var template_row_to_remove = document.getElementById("my-members-row-div");
+                var template_row = template_row_to_remove.cloneNode(true);
+                template_row_to_remove.id = "my-members-row-div-original";
+                //template_row_to_remove.remove();
+
                 data["members"].forEach(function (member) {
                     table.appendChild(template_row);
 
-                    var row = document.getElementById("my-members-row-div-template");
+                    var row = document.getElementById("my-members-row-div");
                     row.id  = "my-members-row-div-" + member["phone_number"];
-                    row.style.display = "block";
 
                     const row_items = [
                         { "id" : "member-list-first-name",  "key" : "first_name"    },
