c812e8c40583dbc8087de963a52a51c74e1f4488
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2021-09-20 23:41:42 +0000

    syntax

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,7 @@
 var gmb_api_version = 'https://mybusinessaccountmanagement.googleapis.com/v1';
 function onGoogleSignIn(googleUser) {
     var access_token = googleUser.getAuthResponse().access_token;
-    console.log(googleUser.getAuthResponse();
+    console.log(googleUser.getAuthResponse());
 
     // Using the sign in data to make a Google My Business API call
     var req = gmb_api_version + '/accounts';
