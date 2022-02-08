74e824bbde808e184b3af756a26c8a1937a9505e
Ubuntu <ubuntu@ip-172-31-40-68.us-east-2.compute.internal>
2022-02-08 15:44:48 +0000

    fixed syntax error

--- profile/settings/integrations.head.js
+++ profile/settings/integrations.head.js
@@ -2,7 +2,7 @@ function start() {
     gapi.load("auth2", function() {
         auth2 = gapi.auth2.init({
             client_id: GAPI_client_id + ".apps.googleusercontent.com",
-            Scopes to request in addition to "profile" and "email"
+            // Scopes to request in addition to "profile" and "email"
             scope: "https://www.googleapis.com/auth/business.manage",
             immediate: true
         });
